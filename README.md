94b5ef57c68b4286500bde29f1890c73db531f14
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-09-30 22:46:14 +0200

    LoRa Radio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # oberhart-embedded
+
+## Entwicklung
+Für das verwendete Board muss vorher der Treiber für die USB to UART Bridge installiert werden.
+Der verwendete Chip ist CP2104, Treiber können [hier](https://download.bastelgarage.ch/?dir=Treiber%2FCP210x) gefunden werden
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <LoRa.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define BAND 866E6
+
+int counter = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1)
+      ;
+  }
+
+  Serial.println("LoRa Initializing OK!");
+  delay(2000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(10000);
 }
